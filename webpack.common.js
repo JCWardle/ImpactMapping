9ecf03ba335ded56d50d2596a2b6de053fa7764a
jordanw <jordancwardle@gmail.com>
2017-10-25 20:58:09 +1100

    Sorted production build.

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,64 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+var path = require('path');
+var webpack = require("webpack");
 
-module.exports = merge(common, {
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  }
-});
+module.exports = {
+  entry: {
+    'main': './src/main.ts',
+    'vendor': './src/vendor.ts'
+  },
+  output: {
+    filename: './[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx','.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        loader: [
+          'ts-loader', 
+          'angular2-template-loader'
+        ],
+        exclude: /node_modules/
+      },
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/, 
+        loader: 'file-loader?limit=100000' 
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader?caseSensitive=true'
+      },
+      { 
+        test: /\.css$/, loader: "style-loader!css-loader?importLoaders=1" 
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader"
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor']
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ]
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,72 +1,17 @@
-var path = require('path');
-var webpack = require("webpack");
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
-function root(args) {
-  var _root = path.resolve(__dirname, '..');
-  args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [_root].concat(args));
-}
-
-module.exports = {
-  entry: {
-    'main': './src/main.ts',
-    'vendor': './src/vendor.ts'
-  },
-  output: {
-    filename: './dist/[name].js'
-  },
-  devtool: "inline-source-map",
-  resolve: {
-    extensions: ['.ts', '.tsx','.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts/,
-        loader: [
-          'ts-loader', 
-          'angular2-template-loader'
-        ],
-        exclude: /node_modules/
-      },
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/, 
-        loader: 'file-loader?limit=100000' 
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader?caseSensitive=true'
-      },
-      { 
-        test: /\.css$/, loader: "style-loader!css-loader?importLoaders=1" 
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-            loader: "style-loader"
-        }, {
-            loader: "css-loader"
-        }, {
-            loader: "sass-loader"
-        }]
-      }
-    ]
-  },
+module.exports = merge(common, {
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
+    new UglifyJSPlugin({
+      sourceMap: true
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor']
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
-    }),
-    new ExtractTextPlugin('[name].css')
+    new webpack.DefinePlugin({
+      'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ]
-};
+});
